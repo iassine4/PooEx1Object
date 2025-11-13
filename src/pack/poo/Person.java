160e1c68f9260122216bf6b5bf9c04086fddedfd
Yassine <yassine.zriba@fms-ea.com>
2025-11-13 11:28:23 +0100

    Add new fils: Person class with getters/setters - TestPerson class

---
+++ src/pack/poo/Person.java
@@ -0,0 +1,74 @@
+package pack.poo;
+
+public class Person {
+
+	public String name;
+	public String lastName;
+	public int age;
+	public String adress;
+	
+	public Person(String name, String lastName, int age, String adress) {
+		this.name = name;
+		this.lastName = lastName;
+		this.age = age;
+		this.adress = adress;
+		
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the adress
+	 */
+	public String getAdress() {
+		return adress;
+	}
+
+	/**
+	 * @param adress the adress to set
+	 */
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+}
---
+++ src/pack/poo/TestPerson.java
@@ -0,0 +1,10 @@
+package pack.poo;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
